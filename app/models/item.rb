183b4298ea5f79d9b41e415e85ba199d08685ae7
taaa0905 <takashinakano0905@gmail.com>
2022-02-03 03:48:42 +0900

    itemモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one    :purchase
+  has_many   :comments
+  has_one_attached :image
+
+  validates :product_name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_charge_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :packaging_day_id, presence: true
+  validates :price, presence: true
+  validates :user, presence: true
+  validates :image, presence: true
 end
