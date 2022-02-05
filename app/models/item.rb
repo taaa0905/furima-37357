d56e8fe3f91df69838e3ff0d60d5dfc539f5156d
taaa0905 <takashinakano0905@gmail.com>
2022-02-05 18:16:11 +0900

    テストコード記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,19 +4,8 @@ class Item < ApplicationRecord
   has_many   :comments
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  belongs_to :condition
-  belongs_to :shipping_charge
-  belongs_to :prefecture
-  belongs_to :packaging_day
-
-  validates :product_name, presence: true
-  validates :explanation, presence: true
-  validates :price, presence: true
-  validates :image, presence: true
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :shipping_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :packaging_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+  belongs_to :category, :condition, :shipping_charge, :prefecture,:packaging_day
+  
+  validates :product_name, :explanation, :price, :image, presence: true
+  validates :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :packaging_day_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品機能' do
+    context '出品ができる場合' do
+      it '全ての項目が入力されている場合' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '出品ができない場合' do
+      it 'product_nameが入力されていなければ登録できない' do
+        @item.product_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Product name can't be blank")
+      end
+      it 'explanationが入力されていなければ登録できない' do
+        @item.explanation = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Explanation can't be blank")
+      end
+      it 'category_idの1を選択されている場合登録できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Category can't be blank")
+      end
+      it 'condition_idの1を選択されている場合登録できない' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Condition can't be blank")
+      end
+      it 'shipping_charge_idの1を選択されている場合登録できない' do
+        @item.shipping_charge_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Shipping charge can't be blank")
+      end
+      it 'prefeture_idの1を選択されている場合登録できない' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Prefecture can't be blank")
+      end
+      it 'packaging_day_idの1を選択されている場合登録できない' do
+        @item.packaging_day_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Packaging day can't be blank")
+      end
+      it 'priceの1を選択されている場合登録できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Price can't be blank")
+      end
+      it 'imageが選択されていない場合登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Image can't be blank")
+      end
+      it 'userが紐づいていない場合登録できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("User must exist")
+      end
+    end
+  end
 end
