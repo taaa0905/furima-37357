08a31a6b469145c722f93222ab9aea181d5415cd
taaa0905 <takashinakano0905@gmail.com>
2022-02-11 12:10:29 +0900

    購入内容の表示2

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,17 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create, :new]
+  def index
+    @purchase = Purchase.new
+    @item = Item.find(params[:item_id])
+  end
+  def new
+    @item = Item.find(params[:item_id])
+  end
+  def create
+  end
+
+  private
+  def purchase_params
+    params.require(:purchase).merge(user_id: current_user.id, item_id: current_item.id)
+  end
 end
