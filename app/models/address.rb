8c7df63a834d7cb269ffa1a6c99d285461812b0c
taaa0905 <takashinakano0905@gmail.com>
2022-02-15 19:47:03 +0900

    不要コードの削除

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,8 +2,4 @@ class Address < ApplicationRecord
   belongs_to :purchase
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
-
-  # validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  # validates :post_code, :city, :block, :tel_number, presence: true
-  # validates :prefecture, numericality: { other_than: 1 , message: "can't be blank" }
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,6 +2,4 @@ class Purchase < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one :address
-
-  # validates :user_id, :item_id, presence: true
 end
